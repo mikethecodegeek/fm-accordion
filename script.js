bebeed31cabbc749d49cb5df3478b79b5f552986
mike <mikethecodegeek@gmail.com>
2021-07-20 15:26:38 -0700

    started desktop design

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const sections = document.getElementsByClassName('question');
 
 Array.from(sections).forEach(s => {
     s.addEventListener('click', (e)=> {
-        const sib = e.target.parentElement.nextSibling.nextSibling;
-        sib.classList.toggle("hide")
+        if (e.target.firstElementChild == null) {
+            const sib = e.target.parentElement.nextSibling.nextSibling;
+            const bold = e.target.parentElement.firstElementChild;
+            bold.classList.toggle('bold');
+            sib.classList.toggle("hide");
+        } else {
+            const sib = e.target.nextSibling.nextSibling;
+            const bold = e.target.firstElementChild;
+            bold.classList.toggle('bold');
+            sib.classList.toggle("hide");
+        }
     })
 })
 
